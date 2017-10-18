8a74d9cf90ab63fc290226a60fc160198157473a
levsthings <bearnode@gmail.com>
2017-10-18 11:22:36 +0300

    configure enzyme and use it with header

--- src/js/components/Header/Header.test.js
+++ src/js/components/Header/Header.test.js
@@ -1,13 +1,8 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import {shallow} from 'enzyme'
 import React from 'React'
 import Header from './Header'
 
 test('should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer()
-    renderer.render(<Header />)
-    expect(renderer.getRenderOutput()).toMatchSnapshot()
+    const wrapper = shallow(<Header />)
+    expect(wrapper).toMatchSnapshot()
 })
-
-/*
-    Enzyme
-*/
