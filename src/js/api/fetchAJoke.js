680419911f744b3bc3c1267ffe4def997d6d7c12
Lev E <bearnode@gmail.com>
2017-10-24 09:47:31 +0300

    update api location

--- src/js/api/fetchAJoke.js
+++ src/js/api/fetchAJoke.js
@@ -1,7 +1,15 @@
 import fetch from 'isomorphic-fetch'
 
+/* Public Key for 100 Chuck Norris facts a day allowance. */
+
+const config = {
+    headers: {
+        'x-api-key': 'ecBldreuzk80wTAQdmfkk2xgVPH7MquX1D8FTndW'
+    }
+}
+
 export default function fetchAjoke() {
-    return fetch('https://hebgae6thk.execute-api.us-east-2.amazonaws.com/dev/random-fact')
+    return fetch('https://0hg1zwamn6.execute-api.us-east-2.amazonaws.com/production/random-fact', config)
         .then(res => res.json())
         .then(res => res.joke.fact)
 }
